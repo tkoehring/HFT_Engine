5df06efe150ec3ce9adb1b25b4c27b50aa2741af
Travis Koehring <travis.koehring@gmail.com>
2026-04-07 16:43:11 -0500

    Jenkins test

---
+++ tests/temp.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <matching/qty.h>
+
+TEST(FormatTest, BadFormatting) {
+
+    auto q = matching::Qty::from_units(10);
+
+    if (q.units() == 10) {
+        EXPECT_TRUE(true);
+    } else {
+        EXPECT_TRUE(false);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        EXPECT_EQ(i, i);
+    }
+}
